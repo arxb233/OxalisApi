fcb9f59ddcd2ab9afc65b08a7d390666ecbea508
ni-xue <mail@nixue.top>
2025-05-12 15:43:18 +0800

    up - 优化改成 先登录 后 签到

--- Job/HayFrpJob.cs
+++ Job/HayFrpJob.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
 using Tool;
+using Tool.Utils;
 
 namespace OxalisApi.Job
 {
@@ -20,17 +21,39 @@ namespace OxalisApi.Job
 
         public async Task Execute(IJobExecutionContext context)
         {
+            //{"type":"login","user":"","passwd":""} 登录
             //{"type":"sign","csrf":""} 签到
             //{"type":"csrf","csrf":""} 不掉线
             //{"type":"info","csrf":""} 获取用户信息
-            List<(string, string)> result = [("sign", "nRdyYRddPljIXSaUMynd"), ("sign", "95Y8fe8DPSS622MOMJ2p")];
-            foreach (var item in result) 
+            List<(string, string)> result = [("nixue", "123456"), ("sign", "95Y8fe8DPSS622MOMJ2p")];
+            foreach (var item in result)
             {
-                await PostAsync(item.Item1, item.Item2);
+                var login = await LoginAsync(item.Item1, item.Item2);
+                if (login["status"] == 200)
+                {
+                    await SignAsync(login["token"]);
+                }
             }
         }
 
-        private async Task PostAsync(string type, string csrf)
+        /// <summary>
+        /// 登录
+        /// </summary>
+        private async Task<JsonVar> LoginAsync(string user, string passwd)
+        {
+            return await PostAsync(new { type = "login", user, passwd });
+        }
+
+        /// <summary>
+        /// 签到
+        /// </summary>
+        private async Task<JsonVar> SignAsync(string csrf)
+        {
+            return await PostAsync(new { type = "sign", csrf });
+        }
+
+
+        private async Task<JsonVar> PostAsync(object json)
         {
             var request = new HttpRequestMessage
             {
@@ -43,19 +66,22 @@ namespace OxalisApi.Job
                     { "User-Agent", "PostmanRuntime-ApipostRuntime/1.1.0" },
                     { "Connection", "keep-alive" },
                 },
-                Content = new StringContent($"{{\"type\":\"{type}\",\"csrf\":\"{csrf}\"}}", new MediaTypeHeaderValue("application/json"))
+                Content = new StringContent(json.ToJson(), new MediaTypeHeaderValue("application/json"))
             };
             using var response = await _httpClient.SendAsync(request);
             response.EnsureSuccessStatusCode();
             var body = await response.Content.ReadAsStringAsync();
 
             StringBuilder builder = new();
-            foreach (var item in body.JsonVar())
+            var jsons = body.JsonVar();
+            foreach (var item in jsons)
             {
                 builder.AppendLine($"{item.Key}: {item.Current.Data}");
             }
 
             _logger.LogInformation("{builder}", builder.ToString());
+
+            return jsons;
         }
     }
 }
