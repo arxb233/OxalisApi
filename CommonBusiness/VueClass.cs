bff9724716bd14ed15267803e961bb7b3d6ac3a8
Baris <2501920571@qq.com>
2025-06-20 10:30:37 +0800

    up

---
+++ CommonBusiness/VueClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HKRM_Server_C.CommonBusiness
+{
+    public class VueClass
+    {
+        public class Option(string value, string? label)
+        {
+            [JsonPropertyName("value")]
+            public string Value { get; set; } = value;
+            [JsonPropertyName("label")]
+            public string? Label { get; set; } = label;
+        }
+
+        public class VanPicker(string? text, List<VanPicker?>? children)
+        {
+            [JsonPropertyName("text")]
+            public string? Text { get; set; } = text;
+            [JsonPropertyName("children")]
+            public List<VanPicker?>? Children { get; set; } = children;
+        }
+    }
+}
--- Controllers/Major/Major.cs
+++ Controllers/Major/Major.cs
@@ -2,6 +2,7 @@
 using AdvancedSharpAdbClient.Models;
 using HKRM_Server_C.CommonBusiness;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient.DataClassification;
 using System;
 using System.Net.Http;
 using System.Text;
@@ -9,69 +10,80 @@ using System.Text.Json;
 using Tool;
 using Tool.SqlCore;
 using Tool.Utils;
-using Tool.Utils.Data;
 using Tool.Web.Api;
 using Tool.Web.Routing;
+using static HKRM_Server_C.CommonBusiness.VueClass;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace OxalisApi.Controllers.Major
 {
     public class Major : MinApi
     {
         [Ashx(State = AshxState.Get)]
-        public async Task<IApiOut> School(string year,string subject)
+        public async Task<IApiOut> School(string year,string subject,string is985,string pici,string province,string prefecture, string county)
         {
             var dbHelper = DbBase.GetDynamicDb("MAJOR");
-            var Proddict = await dbHelper.SelectDictionaryAsync($"select ROW_NUMBER() OVER (ORDER BY 最低分数线 Desc) AS id,学校,省,城市,区,是否985,是否211,是否双一流,录取批次,招生类型,选科要求,最低分数线,最低分段,办学性质 from T甘肃_投档线_2024 where 年份=N'{year}' and 科类=N'{subject}'");
+            string str =$"select ROW_NUMBER() OVER (ORDER BY 最低分数线 Desc) AS id,学校,省,城市,区,是否985,是否211,是否双一流,录取批次,招生类型,选科要求,最低分数线,最低分段,办学性质 from T甘肃_投档线_2024 where 年份=N'{year}' and 科类=N'{subject}'";
+            if (!string.IsNullOrWhiteSpace(is985) && is985 != "全部") {str += $" and 是否985=N'{is985}'";}
+            if (!string.IsNullOrWhiteSpace(pici) && pici !="全部"){ str += $" and 录取批次=N'{pici}'";}
+            if (!string.IsNullOrWhiteSpace(province)  && province != "全部") { str += $" and 省=N'{province}'"; }
+            if (!string.IsNullOrWhiteSpace(prefecture) && prefecture != "全部") { str += $" and 城市=N'{prefecture}'"; }
+            if (!string.IsNullOrWhiteSpace(county) && county != "全部") { str += $" and 区=N'{county}'"; }
+            var Proddict = await dbHelper.SelectDictionaryAsync(str);
             return new JsonOut(new { message = Proddict });
         }
-        private async Task<IApiOut> GetLogoUrl([ApiVal(Val.Service)] IHttpClientFactory clientFactory, [ApiVal(Val.Service)] ILogger<Major> logger)
+        [Ashx(State = AshxState.Get)]
+        public async Task<IApiOut> GetLogoUrl([ApiVal(Val.Service)] IHttpClientFactory clientFactory, [ApiVal(Val.Service)] ILogger<Major> logger,string name)
         {
-<<<<<<< HEAD
             var dbHelper = DbBase.GetDynamicDb("MAJOR");
             var Proddict = await dbHelper.SelectDictionaryAsync($"select name,logourl from SchoolLogo where name=N'{name}'");
             if (Proddict.Count == 0) {
-                return new JsonOut(new { message = "没找到学校", issuccess = false });
-=======
-            var _httpClient = clientFactory.CreateClient("Major");
-            try
-            {
-                var respJson = await _httpClient.GetStringAsync("https://www.urongda.com/data/search.json");
-                var list = JsonSerializer.Deserialize<List<SchoolEntry>>(respJson, new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true
-                });
-                List<Dictionary<string,object>?> base64DataUriList = [];
-                var dbHelper = DbBase.GetDynamicDb("MAJOR");
-                if (list is not null) {
-                    foreach (var (index,match) in list.Index())
-                    {
-                        var slug = match?.EnName?.ToLower().Replace(" ", "-");
-                        var logoUrl = $"https://cdn.urongda.com/images/normal/medium/{slug}-logo-1024px.png";
-
-                        var request = new HttpRequestMessage(HttpMethod.Get, logoUrl);
-                        request.Headers.Referrer = new Uri("https://www.urongda.com/");
-                        var imgResp = await _httpClient.SendAsync(request);
-                        if (!imgResp.IsSuccessStatusCode)
-                        {
-                            continue;
-                        }
-                        var imgBytes = await imgResp.Content.ReadAsByteArrayAsync();
-                        var contentType = imgResp.Content.Headers.ContentType?.MediaType ?? "image/png";
-                        var base64 = Convert.ToBase64String(imgBytes);
-                        var base64DataUri = $"data:{contentType};base64,{base64}";
-                        var dict = new { name = match?.Name, LogoUrl = base64DataUri };
-                        base64DataUriList.Add(dict?.ToDictionary());
-                        await dbHelper.InsertAsync("SchoolLogo", dict);
-                    }
-                }
-                return new JsonOut(new { Message = base64DataUriList, Issuccess = true });
-            }
-            catch (Exception)
-            {
-                return new JsonOut(new { Message = "未找到学校", Issuccess = false });
->>>>>>> parent of f38d88d (学校图标获取)
+                return new JsonOut(new { message = "没找到学校", issuccess = true });
             }
             return new JsonOut(new { message = Proddict[0]["logourl"], issuccess = true });
+        }
+
+        [Ashx(State = AshxState.Get)]
+        public async Task<IApiOut> GetPici()
+        {
+            var dbHelper = DbBase.GetDynamicDb("MAJOR");
+            var Proddict = await dbHelper.SelectDictionaryAsync($"select DISTINCT 录取批次 from T甘肃_投档线_2024 order by 录取批次");
+            if (Proddict.Count == 0)
+            {
+                return new JsonOut(new { message = "没找到批次", issuccess = true });
+            }
+            List<string?> datas = [];
+            foreach (var (index, item)  in Proddict.Index()) {
+                if (index == 0) { datas.Add("全部"); }
+                datas.Add(item["录取批次"]?.ToString());
+            }          
+            return new JsonOut(new { message = datas, issuccess = true });
+        }
+        [Ashx(State = AshxState.Get)]
+        public async Task<IApiOut> GetCitySchool()
+        {
+            var dbHelper = DbBase.GetDynamicDb("MAJOR");
+            var Proddict = await dbHelper.SelectDictionaryAsync($"SELECT DISTINCT 省, 城市, 区 FROM T甘肃_投档线_2024 order by 省, 城市, 区");
+            if (Proddict.Count == 0)
+            {
+                return new JsonOut(new { message = "没找到城市", issuccess = true });
+            }
+            var result = new List<VanPicker>();
+            var groupedByProvince = Proddict.GroupBy(r => r["省"]);
+            foreach (var (provinceindex, provinceGroup) in groupedByProvince.Index())
+            {
+                if (provinceindex == 0){result.Add(new VanPicker("全部", [new VanPicker("全部", [new VanPicker("全部", null)])]));}
+                var provinceNode = new VanPicker(provinceGroup.Key.ToString(), []);
+                var groupedByCity = provinceGroup.GroupBy(r => r["城市"]);
+                foreach (var (Cityindex, cityGroup) in groupedByCity.Index())
+                {
+                    var cityNode = new VanPicker(cityGroup.Key.ToString(), [.. cityGroup.Select(r => r is null ? null : new VanPicker(r["区"]?.ToString(), null))]);
+                    if (Cityindex == 0) { cityNode.Children?.Insert(0,new VanPicker("全部", null)); }
+                    provinceNode.Children?.Add(cityNode);
+                }
+                result.Add(provinceNode);
+            }
+            return new JsonOut(new { message = result, issuccess = true });
         }
     }
     public class SchoolEntry
