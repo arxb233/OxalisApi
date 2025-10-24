c06d1b3671b4441f513f1f37178f2c9345dc3862
Baris <2501920571@qq.com>
2025-10-24 12:00:20 +0800

    up - 添加输出遗留文件检测直接返回

--- CommonBusiness/TgBotClass.cs
+++ CommonBusiness/TgBotClass.cs
@@ -54,53 +54,57 @@ namespace OxalisApi.CommonBusiness
                 {
                     try
                     {
-                        #region
+                        #region Wallet
                         await SendProcess($"任务开始执行....");
                         await SendProcess($"1.正在查询AutodL钱包信息....");
                         var Wallet = await AutoDLClass.Wallet(tb.AutoDL.Authorization);
                         if (Wallet <= 1.5) { await SendProcess($"1.AutoDL余额不足1.5元,请保证余额充足再使用！"); return; }
                         #endregion
 
-                        #region
+                        #region GPU
                         await SendProcess($"2.当前AutoDL账户余额为{Wallet}元,正在查询AutoDL设备信息....");
                         var Check = await AutoDLClass.Check(tb.AutoDL.Authorization, tb.AutoDL.Instance_uuid);
                         if (Check == -1) { await SendProcess($"当前AutoDL实列已存在,请稍后使用！"); return; }
                         if (Check == 0) { await SendProcess($"当前AutoDL实列没有可用GPU,请稍后使用！"); return; }
                         #endregion
 
-                        #region
+                        #region Download
                         await SendProcess($"3.当前AutoDL实列可用GPU为{Check}个,满足运行条件！");
                         await SendProcess($"4.视频链接获取成功，正在下载视频，请耐心等待....");
                         using var video = await VideoDownClass.DownLoad(tb.DownApi.DownApiUrl, MatchUrl.Value);
                         if (video == Stream.Null || video.Length <= 333) { await SendProcess("视频获取失败！"); return; }
                         #endregion
 
-                        #region
+                        #region Power_on
                         await SendProcess($"5.视频下载成功,正在进行开机,预计30s....");
                         var Open = await AutoDLClass.Open(tb.AutoDL.Authorization, tb.AutoDL.Instance_uuid, tb.AutoDL.Payload);
                         if (!Open) { await SendProcess($"当前AutoDL实列开机失败,请联系管理员！"); return; }
                         do { if (await AutoDLClass.Check(tb.AutoDL.Authorization, tb.AutoDL.Instance_uuid) == -1) { break; }; await Task.Delay(5000); } while (true);
                         #endregion
 
-                        #region
+                        #region SSH
                         await SendProcess($"6.开机成功,正在进行远程链接....");
                         using var sshHelper = new LinuxSshHelper(tb.AutoDL.Host, tb.AutoDL.Port, tb.AutoDL.Username, tb.AutoDL.Password);
                         sshHelper.Connect(); sshHelper.OpenPort(tb.ComfyUI.Port);
-                        await SendProcess($"7.远程链接成功,正在上传视频....");
-                        sshHelper.UploadStream(video, tb.ComfyUI.InputPath);
-                        await SendProcess($"8.视频上传成功,正在获取工作流....");
+                        await SendProcess($"8.远程链接成功,正在获取工作流....");
                         using var PromptStream = sshHelper.DownloadStream(tb.ComfyUI.Prompt);
-                        if (sshHelper.FileExists(tb.ComfyUI.OutputPath[0])) { await SendProcess("输出文件已存在,请检查删除后重新执行！"); await AutoDLClose(); return; }
+                        if (sshHelper.FileExists(tb.ComfyUI.OutputPath[0]))
+                        {
+                            await SendProcess("输出文件已存在,取消当前任务，返回上条任务文件数据！");
+                            await OutPutMessage(sshHelper); return;
+                        }
+                        await SendProcess($"7.工作流获取成功,正在上传视频....");
+                        sshHelper.UploadStream(video, tb.ComfyUI.InputPath);
                         #endregion
 
-                        #region
+                        #region ComfyUI
                         await SendProcess($"9.工作流获取成功,正在启动服务并执行工作流....");
                         ComfyUIClass comfyUIClass = new($"127.0.0.1:{tb.ComfyUI.Port}", tb.ComfyUI.Prompt, PromptStream.ToArray().ToByteString(), (_msg) => SendProcess(_msg));
                         do { if (await comfyUIClass.GetPrompt() is (bool, int) GetPromptResult && GetPromptResult.Item1) { break; } await Task.Delay(TimeSpan.FromSeconds(10)); } while (true);
                         if (await comfyUIClass.Prompt() is (bool, string) PromptResult && !PromptResult.Item1) { await SendProcess(PromptResult.Item2); await AutoDLClose(); return; }
                         #endregion
 
-                        #region
+                        #region Prompt
                         await SendProcess($"10.工作流执行成功,正在获取工作流执行状态....");
                         do
                         {
@@ -114,18 +118,9 @@ namespace OxalisApi.CommonBusiness
                         } while (true);//await comfyUIClass.Websocket();//await comfyUIClass.Task;
                         #endregion
 
-                        #region
-                        await SendProcess($"11.工作流执行完成,正在下载生成的视频....");
-                        using var AIvideo = sshHelper.DownloadStream(tb.ComfyUI.OutputPath[0]);
-                        if (AIvideo == Stream.Null || AIvideo.Length == 0) { await SendProcess("视频获取失败！"); await AutoDLClose(); return; }
-                        await Ext.DownloadFileAsStreamAsync(AIvideo, tb.TgBot.CacheFile);
-                        using var InputVideo = sshHelper.DownloadStream(tb.ComfyUI.InputPath);
-                        if (InputVideo == Stream.Null || InputVideo.Length == 0) { await SendProcess("视频获取失败！"); await AutoDLClose(); return; }
-                        await _bot.SendVideo(tb.TgBot.ChatVideoId, InputVideo, msg.Text);
-                        foreach (var path in tb.ComfyUI.OutputPath) { sshHelper.DeleteFile(path); }
+                        #region OutPut
+                        await OutPutMessage(sshHelper);
                         #endregion
-                        await SendProcess($"12.任务执行成功,正在关闭实例....");
-                        await AutoDLClose();
                     }
                     catch (Exception ex)
                     {
@@ -139,6 +134,19 @@ namespace OxalisApi.CommonBusiness
                 {
                     stringBuilder.AppendLine(text);
                     return await _bot.EditMessageText(msg.Chat.Id, StartMessage.Id, stringBuilder.ToString());
+                }
+                async Task OutPutMessage(LinuxSshHelper ssh)
+                {
+                    await SendProcess($"11.工作流执行完成,正在下载生成的视频....");
+                    using var AIvideo = ssh.DownloadStream(tb.ComfyUI.OutputPath[0]);
+                    if (AIvideo == Stream.Null || AIvideo.Length == 0) { await SendProcess("视频获取失败！"); await AutoDLClose(); return; }
+                    await Ext.DownloadFileAsStreamAsync(AIvideo, tb.TgBot.CacheFile);
+                    using var InputVideo = ssh.DownloadStream(tb.ComfyUI.InputPath);
+                    if (InputVideo == Stream.Null || InputVideo.Length == 0) { await SendProcess("视频获取失败！"); await AutoDLClose(); return; }
+                    await _bot.SendVideo(tb.TgBot.ChatVideoId, InputVideo, msg.Text);
+                    foreach (var path in tb.ComfyUI.OutputPath) { ssh.DeleteFile(path); }
+                    await SendProcess($"12.任务执行成功,正在关闭实例....");
+                    await AutoDLClose();
                 }
                 async Task AutoDLClose()
                 {
@@ -154,8 +162,9 @@ namespace OxalisApi.CommonBusiness
                     await _bot.SendVideo(msg.Chat.Id, _outputStream, "", ParseMode.None, msg);
                     Ext.DeleteFile(tb.TgBot.CacheFile);
                 }
-                catch {
-                    await _bot.SendMessage(msg.Chat.Id,"缓存文件获取或删除失败！", ParseMode.None, msg);
+                catch
+                {
+                    await _bot.SendMessage(msg.Chat.Id, "缓存文件获取或删除失败！", ParseMode.None, msg);
                 }
             }
         }
